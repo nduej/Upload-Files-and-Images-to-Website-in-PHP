a8085175d0a13ba9a2b52ca67a7aa643f97f4006
Michael Ndue <95193983+nduej@users.noreply.github.com>
2023-03-14 11:37:22 +0100

    Add files via upload

---
+++ UploadFiles&ImagesPhp/uploads.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['submit'])) {
+    $file = $_FILES['file'];
+    
+    $fileName = $_FILES['file']['name'];
+    $fileTmpName = $_FILES['file']['tmp_name'];
+    $fileSize = $_FILES['file']['size'];
+    $fileError = $_FILES['file']['error'];
+    $fileType = $_FILES['file']['type'];
+
+    $fileExt = explode('.', $fileName);
+    $fileActualExt = strtolower(end($fileExt));
+
+    $allowed = array('jpg', 'jpeg', 'png', 'webp', 'pdf');
+
+    if (in_array($fileActualExt, $allowed)) {
+        if ($fileError === 0) {
+            if ($fileSize < 1000000) {
+                $fileNameNew = uniqid('', true).".".$fileActualExt;
+                $fileDestination = 'uploads/'.$fileNameNew;
+                move_uploaded_file($fileTmpName, $fileDestination);
+                header("Location: index.php?uploadsuccess");
+            } else {
+                echo "File Size too Large!";
+            }
+        } else {
+            echo "Error Uploading File!";
+        }
+    } else {
+        echo "This File Type is not Allowed!";
+    }
+}
